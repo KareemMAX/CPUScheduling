6bc9a3267b5bab477899654bd237d24fd08ebdf0
andrew <andrewnasief3.com>
2021-12-29 00:17:23 +0200

    waiting timeList added

--- src/algorithms/Priority_Scheduling.java
+++ src/algorithms/Priority_Scheduling.java
@@ -9,13 +9,16 @@ import java.util.List;
 public class Priority_Scheduling {
     private final AlgorithmAnswer algorithmAnswer;
     private final List<Process> processes;
+    List<Integer> waitingTimesList;
 
     public Priority_Scheduling() {
+        waitingTimesList=new ArrayList<>();
         algorithmAnswer = new AlgorithmAnswer();
         processes = new ArrayList<>();
     }
 
     public Priority_Scheduling(List<Process> processes) {
+        waitingTimesList=new ArrayList<>();
         this.processes = processes;
         algorithmAnswer = new AlgorithmAnswer();
     }
@@ -48,17 +51,18 @@ public class Priority_Scheduling {
         int burstTime=0;
         while (processes.size() > 0) {
             int index = findHighPriority();
+            waitingTimesList.add(burstTime);
             burstTime+=processes.get(index).getBurstTime();
             algorithmAnswer.addProcess(processes.get(index), burstTime);
             processes.remove(index);
         }
+        algorithmAnswer.setWaitingTimesList(waitingTimesList);
     }
     public void calculatePrioritySchedulingWithoutStarvation(){
         int burstTime=0;
         int count=0;
         int index;
         while (processes.size() > 0) {
-
             if(count==3){
                 count=0;
                 index=findLowPriority();
@@ -66,10 +70,12 @@ public class Priority_Scheduling {
                 count++;
                 index = findHighPriority();
             }
+            waitingTimesList.add(burstTime);
             burstTime+=processes.get(index).getBurstTime();
             algorithmAnswer.addProcess(processes.get(index), burstTime);
             processes.remove(index);
         }
+        algorithmAnswer.setWaitingTimesList(waitingTimesList);
     }
 }
 
