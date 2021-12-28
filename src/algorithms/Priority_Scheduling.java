d63731b524f85b5be89b210d9f20947ccf5e5a0d
andrew <andrewnasief3.com>
2021-12-29 00:17:40 +0200

    waiting timeList added

--- src/algorithms/Priority_Scheduling.java
+++ src/algorithms/Priority_Scheduling.java
@@ -12,32 +12,35 @@ public class Priority_Scheduling {
     List<Integer> waitingTimesList;
 
     public Priority_Scheduling() {
-        waitingTimesList=new ArrayList<>();
+        waitingTimesList = new ArrayList<>();
         algorithmAnswer = new AlgorithmAnswer();
         processes = new ArrayList<>();
     }
 
     public Priority_Scheduling(List<Process> processes) {
-        waitingTimesList=new ArrayList<>();
+        waitingTimesList = new ArrayList<>();
         this.processes = processes;
         algorithmAnswer = new AlgorithmAnswer();
     }
+
     public void addProcess(Process process) {
         processes.add(process);
     }
-    private int findHighPriority(){
+
+    private int findHighPriority() {
         int mini = (int) 1e9;
         int index = 0;
         for (int j = 0; j < processes.size(); j++) {
-                if (mini > processes.get(j).getPriority()) {
-                    index = j;
-                    mini = processes.get(j).getPriority();
-                }
+            if (mini > processes.get(j).getPriority()) {
+                index = j;
+                mini = processes.get(j).getPriority();
             }
+        }
         return index;
     }
-    private int findLowPriority(){
-        int max =0;
+
+    private int findLowPriority() {
+        int max = 0;
         int index = 0;
         for (int i = 0; i < processes.size(); i++) {
             if (max < processes.get(i).getPriority()) {
@@ -47,31 +50,33 @@ public class Priority_Scheduling {
         }
         return index;
     }
-    public void calculatePrioritySchedulingWithStarvation(){
-        int burstTime=0;
+
+    public void calculatePrioritySchedulingWithStarvation() {
+        int burstTime = 0;
         while (processes.size() > 0) {
             int index = findHighPriority();
             waitingTimesList.add(burstTime);
-            burstTime+=processes.get(index).getBurstTime();
+            burstTime += processes.get(index).getBurstTime();
             algorithmAnswer.addProcess(processes.get(index), burstTime);
             processes.remove(index);
         }
         algorithmAnswer.setWaitingTimesList(waitingTimesList);
     }
-    public void calculatePrioritySchedulingWithoutStarvation(){
-        int burstTime=0;
-        int count=0;
+
+    public void calculatePrioritySchedulingWithoutStarvation() {
+        int burstTime = 0;
+        int count = 0;
         int index;
         while (processes.size() > 0) {
-            if(count==3){
-                count=0;
-                index=findLowPriority();
-            }else {
+            if (count == 3) {
+                count = 0;
+                index = findLowPriority();
+            } else {
                 count++;
                 index = findHighPriority();
             }
             waitingTimesList.add(burstTime);
-            burstTime+=processes.get(index).getBurstTime();
+            burstTime += processes.get(index).getBurstTime();
             algorithmAnswer.addProcess(processes.get(index), burstTime);
             processes.remove(index);
         }
